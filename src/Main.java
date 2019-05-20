f0c4ab9dced0ab43d24c5f618009963d6880a594
Annina Henninger <annina.henninger@aon.at>
2019-05-20 09:15:36 +0200

    once more

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+
+/**
+ * Create a program to create a new array list, containing 6 String objects with names of Austrian cities and print out
+ * the collection.
+ *
+ *     Then, create a new ArrayList containing current temperatures (in Celsius) in this cities. Print out this second
+ *     collection.
+ *
+ *     Try to join the data from those two array list in a single object of the class City. add those object to the
+ *     array list, and then print the collection.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String s1 = "Salzburg";
+        String s2 = "Wien";
+        String s3 = "Klagenfurt";
+        String s4 = "Graz";
+        String s5 = "Bregenz";
+        String s6 = "Linz";
+
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add(s1);
+        cities.add(s2);
+        cities.add(s3);
+        cities.add(s4);
+        cities.add(s5);
+        cities.add(s6);
+
+        for(String)
     }
 }
